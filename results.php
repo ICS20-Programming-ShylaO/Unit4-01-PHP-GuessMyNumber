2f21beb27ac031905d65cf94efebb46283f82e46
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-04-14 14:11:03 +0000

    updated results

--- results.php
+++ results.php
@@ -1,23 +1,20 @@
   <?php
-	// intializing constants and variables
+  // intializing constants and variables
   const MIN = 1;
   const MAX = 6;
   $userGuess = $_POST['user-guess'];
-  $randomNumber = rand(MIN,MAX);
+  $randomNumber = rand(MIN, MAX);
 
   // if userGuess == randomNumber, display "You got it right!"
+  if ($userGuess == $randomNumber) {
 
-if ($userGuess == $randomNumber) {
-    
- echo 'You got it right!'; 
-
+      echo 'You got it right!';
 }
-
+  
   // if userGuess != randomNumber, display "wrong guess, try again."
 if ($userGuess != $randomNumber) {
-
     echo 'Wrong guess, try again.';
       
 }
-  
+    
 ?>
